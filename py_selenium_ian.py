b079f16daf943ce5e5d1d4e6239729c170acaa29
Emmanuel Nosa Evbuomwan <emmanuelnosa.e@gmail.com>
2017-12-12 15:28:55 +0100

    refactored xpath string

--- py_selenium_ian.py
+++ py_selenium_ian.py
@@ -1,16 +1,17 @@
-#https://www.github.com/iamnosa
-#Let's import the Selenium package
+# https://www.github.com/nosahama/whatsapp-web-bot
+
 from selenium import webdriver
 
-#Let's use Firefox as our browser
+# Let's use Firefox as our browser, but we can also use Chrome, etc.
 web = webdriver.Firefox()
 web.get('http://web.whatsapp.com')
 input()
 
-#Replace Mr Kelvin with the name of your friend to spam
-elem = web.find_element_by_xpath('//span[contains(text(),"Mr Kelvin")]')
+# Replace Mr Kelvin with the name of your friend to text
+friend_name = 'Mr Kelvin'
+elem = web.find_element_by_xpath('//span[contains(text(),{friend_name})]'.format(friend_name=friend_name))
 elem.click()
 elem1 = web.find_elements_by_class_name('input')
 while True:
-	elem1[1].send_keys('hahahahahahaha')
-	web.find_element_by_class_name('send-container').click()
+    elem1[1].send_keys('hahahahahahaha')
+    web.find_element_by_class_name('send-container').click()
